fe9d6591856c71d9b3cd37ffde8c2c5c34adab7f
Akshay <akshaydhumda@gmail.com>
2026-09-01 13:44:20 +0530

    init modified'

--- vault/backend/app/generators/__init__.py
+++ vault/backend/app/generators/__init__.py
@@ -1,8 +1,9 @@
-from . import anomaly, classification, clustering, regression
+from . import anomaly, classification, clustering, mystery, regression
 
 GENERATORS = {
     "classification": classification.generate,
     "regression": regression.generate,
     "clustering": clustering.generate,
     "anomaly": anomaly.generate,
+    "mystery": mystery.generate,
 }
